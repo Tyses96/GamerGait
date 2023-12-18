4fefae86db02be28727c3994b8604af38b28967c
Tyses96 <83593245+Tyses96@users.noreply.github.com>
2023-12-18 19:23:33 +0000

    added pagination working

--- src/main/web/html/index.js
+++ src/main/web/html/index.js
@@ -1,23 +1,30 @@
 // JavaScript code
 const gridContainer = document.querySelector("#grid-container");
 const steamGameDataUrl = "http://localhost:8080/gameDetails/"
-
+let pageNumber = 0;
+let maxPages = 0;
 
 function searchGames(){
 	x = document.getElementById("searchbar");
-        fetch('http://localhost:8080/games/search=' + x.value.toString())
+    let pageDetails;
+        fetch('http://localhost:8080/games/search=' + x.value.toString() + "?page=" + pageNumber)
         .then(response => response.json())
-        .then(data => createCards(data))
+        .then(data => {createCards(data)})
 }
+
+
 
  function createCards(page_of_games){
     removeCards(gridContainer);
+
     page_of_games.content.forEach(async(item) => {
         const itemCard = document.createElement("div");
         itemCard.classList.add("item-card");
         itemCard.classList.add("centre");
-        const itemName = document.createElement("h1");
+        const itemName = document.createElement("h4");
         const itemPicture = document.createElement("img");
+
+        maxPages = page_of_games.totalPages;
 
         itemPicture.classList.add("card-image");
 
@@ -35,6 +42,30 @@ function searchGames(){
         itemCard.appendChild(itemPicture)
         gridContainer.appendChild(itemCard)
     });
+}
+
+function incrementPageNumber(){
+    window.scrollTo({ top: 0, behavior: 'smooth' })
+    if(pageNumber >= maxPages){
+        pageNumber = maxPages;
+    }
+    else{
+         pageNumber += 1;
+         window.scrollTo({ top: 0, behavior: 'smooth' });
+         searchGames();
+    }
+}
+
+function decrementPageNumber(){
+
+    if(pageNumber <= 0){
+        pageNumber = 0;
+    }
+    else {
+        pageNumber -= 1;
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+        searchGames();
+    }
 }
 
 function removeCards(parent){
